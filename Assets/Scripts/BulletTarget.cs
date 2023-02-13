ff3d55fae771c8ca427b803758b9fd3b9eda7016
nullsoftware <tumyrthebot@gmail.com>
2023-02-13 06:26:20 +0200

    Updated enemy.

--- Assets/Scripts/BulletTarget.cs
+++ Assets/Scripts/BulletTarget.cs
@@ -6,13 +6,27 @@ using UnityEngine.Events;
 public class BulletTarget : MonoBehaviour
 {
     [SerializeField] private float _healthPoints = 100;
+    [SerializeField] private UnityEvent _onDamagedEvent = new UnityEvent();
+    [SerializeField] private UnityEvent _onDeathEvent = new UnityEvent();
 
     private float _maxHealth;
 
-    public UnityAction OnDeath;
+    public event UnityAction OnDamaged
+    {
+        add => _onDamagedEvent?.AddListener(value);
+        remove => _onDamagedEvent?.RemoveListener(value);
+    }
+
+    public event UnityAction OnDeath
+    {
+        add => _onDeathEvent?.AddListener(value);
+        remove => _onDeathEvent?.RemoveListener(value);
+    }
 
     public float HealthPoints => _healthPoints;
     public float MaxHealth => _maxHealth;
+
+    public bool IsDead => _healthPoints == 0;
 
     private void Start()
     {
@@ -21,13 +35,22 @@ public class BulletTarget : MonoBehaviour
 
     public void DealDamage(float damage)
     {
+        if (IsDead)
+        {
+            return;
+        }
+
         damage = Mathf.Max(0, damage);
 
         _healthPoints -= Mathf.Min(damage, _healthPoints);
 
         if (_healthPoints == 0)
         {
-            OnDeath?.Invoke();
+            _onDeathEvent?.Invoke();
+        }
+        else
+        {
+            _onDamagedEvent.Invoke();
         }
     }
 
--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,3 +1,4 @@
+using CartoonFX;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -6,48 +7,109 @@ using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.UIElements;
 
+[RequireComponent(typeof(MeshRenderer))]
 [RequireComponent(typeof(Rigidbody))]
 [RequireComponent(typeof(FieldOfView))]
 [RequireComponent(typeof(NavMeshAgent))]
+[RequireComponent(typeof(BulletTarget))]
 public class Enemy : MonoBehaviour
 {
     [SerializeField] private float _rotationSpeed = 1;
+    [SerializeField] private ParticleSystem _lowHealthEffect;
+    [SerializeField] private ParticleSystem _hitEffect;
+    [SerializeField] private ParticleSystem _shieldEffect;
+    [SerializeField] private ParticleSystem _explosionEffect;
+    [SerializeField] private ParticleSystem _fireEffect;
 
+    private MeshRenderer _meshRenderer;
     private Rigidbody _rigidbody;
     private FieldOfView _fieldOfView;
     private NavMeshAgent _meshAgent;
-
-    private float _currentRotatioAngle = 0;
+    private BulletTarget _healthInfo;
 
     private void Start()
     {
+        _meshRenderer = GetComponent<MeshRenderer>();
         _rigidbody = GetComponent<Rigidbody>();
         _fieldOfView = GetComponent<FieldOfView>();
         _meshAgent = GetComponent<NavMeshAgent>();
+        _healthInfo = GetComponent<BulletTarget>();
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        _meshAgent.enabled = true;
+        if (_healthInfo.HealthPoints > 0)
+        {
+            _meshAgent.enabled = true;
+            _rigidbody.isKinematic = true;
+        }
     }
 
     private void Update()
     {
-        if (_fieldOfView.CanSeePlayer && _meshAgent.enabled)
+        if (_meshAgent.enabled)
         {
-            //float distance = Vector3.Distance(_fieldOfView.Target.transform.position, transform.position);
+            if (_fieldOfView.CanSeePlayer)
+            {
+                float distance = Vector3.Distance(_fieldOfView.Target.transform.position, transform.position);
 
-            var targetRotation = Quaternion.LookRotation(_fieldOfView.Target.transform.position - transform.position);
+                var targetRotation = Quaternion.LookRotation(_fieldOfView.Target.transform.position - transform.position);
 
-            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, _rotationSpeed * Time.deltaTime);
+                transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, _rotationSpeed * Time.deltaTime);
 
-            if (_meshAgent.destination != _fieldOfView.Target.transform.position)
-                _meshAgent.SetDestination(_fieldOfView.Target.transform.position);
+                if (_meshAgent.destination != _fieldOfView.Target.transform.position)
+                    _meshAgent.SetDestination(_fieldOfView.Target.transform.position);
+            }
+            else
+            {
+                transform.Rotate(Vector3.up, 40f * Time.deltaTime);
+                // idle patrol
+            }
         }
-        else
+    }
+
+    public void OnDamaged()
+    {
+        if (_healthInfo.HealthPoints < 15 && !_lowHealthEffect.isPlaying)
         {
-            transform.Rotate(Vector3.up, 40f * Time.deltaTime);
-            // idle patrol
+            _hitEffect.gameObject.SetActive(true);
+            _lowHealthEffect.gameObject.SetActive(true);
+
+            StartCoroutine(SelfDestruct());
         }
+    }
+
+    public void OnDeath()
+    {
+        _meshAgent.enabled = false;
+        _rigidbody.isKinematic = false;
+        _rigidbody.freezeRotation = false;
+        _rigidbody.constraints = RigidbodyConstraints.None;
+        _shieldEffect.gameObject.SetActive(false);
+        _lowHealthEffect.gameObject.SetActive(false);
+        _explosionEffect.gameObject.SetActive(true);
+        _hitEffect.gameObject.SetActive(false);
+        _meshRenderer.enabled = false;
+
+        Destroy(_shieldEffect.gameObject);
+        Destroy(_lowHealthEffect.gameObject);
+
+        DestroyAfterDelay(2);
+    }
+
+    private IEnumerator SelfDestruct()
+    {
+        yield return new WaitForSeconds(3);
+        _shieldEffect.gameObject.SetActive(true);
+
+        yield return new WaitForSeconds(3);
+
+        _healthInfo.DealDamage(_healthInfo.HealthPoints);
+    }
+
+    private IEnumerator DestroyAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        Destroy(gameObject);
     }
 }
